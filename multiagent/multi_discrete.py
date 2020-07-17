224c49b3108505bad2bcf91596e9a94b0374781b
Raphael Avalos <raphael@avalos.fr>
2020-07-17 16:25:00 +0200

    Fix for current gym and add wrapper for RLlib.

--- multiagent/multi_discrete.py
+++ multiagent/multi_discrete.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 import gym
-from gym.spaces import prng
+
 
 class MultiDiscrete(gym.Space):
     """
@@ -26,11 +26,13 @@ class MultiDiscrete(gym.Space):
         self.low = np.array([x[0] for x in array_of_param_array])
         self.high = np.array([x[1] for x in array_of_param_array])
         self.num_discrete_space = self.low.shape[0]
+        self.np_random = None
+        self.seed()
 
     def sample(self):
         """ Returns a array with one sample from each discrete action space """
         # For each row: round(random .* (max - min) + min, 0)
-        random_array = prng.np_random.rand(self.num_discrete_space)
+        random_array = self.np_random.rand(self.num_discrete_space)
         return [int(x) for x in np.floor(np.multiply((self.high - self.low + 1.), random_array) + self.low)]
     def contains(self, x):
         return len(x) == self.num_discrete_space and (np.array(x) >= self.low).all() and (np.array(x) <= self.high).all()
---
+++ multiagent/rllibwrapper.py
@@ -0,0 +1,80 @@
+# From https://github.com/wsjeon/maddpg-rllib/
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from gym.spaces import Discrete, Box, MultiDiscrete
+from ray import rllib
+from make_env import make_env
+
+import numpy as np
+import time
+
+
+class RLlibMultiAgentParticleEnv(rllib.MultiAgentEnv):
+    """Wraps OpenAI Multi-Agent Particle env to be compatible with RLLib multi-agent."""
+
+    def __init__(self, **mpe_args):
+        """Create a new Multi-Agent Particle env compatible with RLlib.
+        Arguments:
+            mpe_args (dict): Arguments to pass to the underlying
+                make_env.make_env instance.
+        Examples:
+            >>> from rllib_env import RLlibMultiAgentParticleEnv
+            >>> env = RLlibMultiAgentParticleEnv(scenario_name="simple_reference")
+            >>> print(env.reset())
+        """
+
+        self._env = make_env(**mpe_args)
+        self.num_agents = self._env.n
+        self.agent_ids = list(range(self.num_agents))
+
+        self.observation_space_dict = self._make_dict(self._env.observation_space)
+        self.action_space_dict = self._make_dict(self._env.action_space)
+
+    def reset(self):
+        """Resets the env and returns observations from ready agents.
+        Returns:
+            obs_dict: New observations for each ready agent.
+        """
+
+        obs_dict = self._make_dict(self._env.reset())
+        return obs_dict
+
+    def step(self, action_dict):
+        """Returns observations from ready agents.
+        The returns are dicts mapping from agent_id strings to values. The
+        number of agents in the env can vary over time.
+        Returns:
+            obs_dict:
+                New observations for each ready agent.
+            rew_dict:
+                Reward values for each ready agent.
+            done_dict:
+                Done values for each ready agent.
+                The special key "__all__" (required) is used to indicate env termination.
+            info_dict:
+                Optional info values for each agent id.
+        """
+
+        actions = [action_dict[agent_id] for agent_id in self.agent_ids]
+        obs_list, rew_list, done_list, info_list = self._env.step(actions)
+
+        obs_dict = self._make_dict(obs_list)
+        rew_dict = self._make_dict(rew_list)
+        done_dict = self._make_dict(done_list)
+        done_dict["__all__"] = all(done_list)
+        # FIXME: Currently, this is the best option to transfer agent-wise termination signal without touching RLlib code hugely.
+        # FIXME: Hopefully, this will be solved in the future.
+        info_dict = self._make_dict([{"done": done} for done in done_list])
+
+        return obs_dict, rew_dict, done_dict, info_dict
+
+    def render(self, mode='human'):
+        time.sleep(0.05)
+        self._env.render(mode=mode)
+
+    def _make_dict(self, values):
+        return dict(zip(self.agent_ids, values))
+
